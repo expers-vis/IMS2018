19559b340c49306340d7424c164db811a253fb86
Karel Klecka <kkleckla@seznam.cz>
2018-11-07 00:23:59 +0100

    stats

--- ims.cpp
+++ ims.cpp
@@ -11,7 +11,7 @@
 
 #define MIN *1
 #define HOD *60
-#define POCET_FILTRU 3
+#define POCET_FILTRU 1
 
 #define KAPACITA_LISU 1000 //kg
 #define SANCE_NA_KVALITNI_REPKU 0.995
@@ -58,6 +58,9 @@ class PanenskyOlej : public Process {
 		Seize(kontrolorKvality);
 		Wait(Uniform(10 MIN, 20 MIN));
 		Release(kontrolorKvality);
+
+		stPanenskyOlej(100);
+        Print("Day: %03d, %02d:%02d : Panensky olej\n" ,((int)Time/1440), ((int)Time/60)%24, ((int)Time)%60);
 	}
 };
 
@@ -76,13 +79,16 @@ class StolniOlej : public Process { // 100 l oleje
 		Seize(kontrolorKvality);
 		Wait(Uniform(10 MIN, 20 MIN));
 		Release(kontrolorKvality);
+
+		stStolniOlej(100);
+        Print("Day: %03d, %02d:%02d : Stolni olej\n" ,((int)Time/1440), ((int)Time/60)%24, ((int)Time)%60);
 	}
 };
 
 class Vylisky : public Process {
 
 	void Behavior() {
-
+		stVylisky(700);
 	}
 };
 
@@ -91,12 +97,12 @@ class Repka : public Process {
 	void Behavior() {
 
 	Enter(Lis1, 1);
-	Wait( Normal(30, 1));
+	Wait( Normal(29 MIN, 1 MIN));
 	(new PanenskyOlej)->Activate();
 	Leave(Lis1, 1);
 
 	Enter(Lis2, 1);
-	Wait( Normal(40, 1));
+	Wait( Normal(38 MIN, 1 MIN));
 	(new StolniOlej)->Activate();
 	(new StolniOlej)->Activate();
 	(new Vylisky)->Activate();
@@ -129,17 +135,37 @@ class Generator : public Event {
 	}
 };
 
+
+class EveryHour : public Event 
+{
+    void Behavior() 
+    {
+        Print("Day: %03d, %02d:%02d : Stolni olej\n" ,((int)Time/1440), ((int)Time/60)%24, ((int)Time)%60);
+        Print("Day: %03d, %02d:%02d : Lis1 fronta: %d\n" ,((int)Time/1440), ((int)Time/60)%24, ((int)Time)%60 ,Lis1.QueueLen());
+        Print("Day: %03d, %02d:%02d : Lis2 fronta: %d\n" ,((int)Time/1440), ((int)Time/60)%24, ((int)Time)%60 ,Lis2.QueueLen());
+        Print("Day: %03d, %02d:%02d : Filtr fronta: %d\n" ,((int)Time/1440), ((int)Time/60)%24, ((int)Time)%60 ,Filtr.QueueLen());
+        Print("Day: %03d, %02d:%02d : RafinacniJednotka fronta: %d\n" ,((int)Time/1440), ((int)Time/60)%24, ((int)Time)%60 ,RafinacniJednotka.QueueLen());
+        Activate(Time+60); // aktivace za 1h
+    }
+};
+
 int main() {
 	RandomSeed(time(NULL));
 	SetOutput("x1.out");
 	Print("Model vyroby repkoveho oleje\n");
 
-	Init(0, 8 HOD);	// inicializace experimentu
+	Init(0, 80 HOD);	// inicializace experimentu
 	(new Generator)->Activate();	// aktivace generatoru pozadavku
+    (new EveryHour)->Activate();
 	
 	Run();	// simulace
 
+	Print("Stolni olej: \n");
 	stStolniOlej.Output();
+	Print("Panensky olej: \n");
+	stPanenskyOlej.Output();
+	Print("Vylisky: \n");
+	stVylisky.Output();
 
 	// tisk vysledku
 	/*Print("Vydestilovano alkoholu celkem: ");
